4fd7cc14d04e121cf9e1ba4ff9ff11ae9541ed8a
NielAgenbag <niel.agenbag@vulpes-tech.co.za>
2024-12-18 20:12:56 +0200

    Transport aircraft first version

--- AircraftControl.py
+++ AircraftControl.py
@@ -1,8 +1,8 @@
 #%% Import alle modules
 import numpy as np
-import math
-
-
+import math as math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 #%%
 # Figure 3.3-1 Computer model of a transport aircraft
@@ -23,7 +23,13 @@ vliegtuig = {'S' : 2170.0,
              'CMQ' : -16.0,
              'CMADOT' : -6.0,
              'CMDE' : -0.016,
-             'RTOD' : 57.29578
+             'RTOD' : 57.29578,
+             'CL0' : 0.20,
+             'CD0' : 0.016,
+             'CM0' : 0.05,
+             'DCDG' : 0.0,
+             'DCMG' : 0.0,
+             'XCG' : 0.28
              }
 
 
@@ -31,53 +37,68 @@ vliegtuig = {'S' : 2170.0,
 
 # Atmosferiese model
 
-def adc(VT, H, AMACH, QBAR):
-    QBAR = 1
+def adc(VT, ALT, AMACH):
+    R0 = 2.377e-3 # Sea level density
+    TFAC = 1.0 - 0.703e-5*ALT
+    
+    T = 519*TFAC
+
+    if ALT > 35000.0:
+        T = 390.0 # Temperature
+    
+    RHO = R0*(TFAC**4.14) # Density
+    AMACH = VT/(1.4*1716.3*T)**0.5 # Mach number
+    QBAR = 0.5*RHO*VT**2 # Dynamic pressure
+
+    PS = 1715.0*RHO*T # Static pressure
 
     return QBAR
 
+
+#%%
+
 # Vliegtuig funksie
 
-def f(v, t, x, xd, land, THTL, ELEV):
-    VT = x(0) # True airspeed (ft/s)
-    ALPHA = x(1)*v['RTOD']
-    THETA = x(2)
-    Q = x(3)
-    H = x(4)
+def f(x, t, v, land, THTL, ELEV):
+    VT = x[0] # True airspeed (ft/s)
+    ALPHA = x[1]*v['RTOD']
+    THETA = x[2]
+    Q = x[3]
+    H = x[4]
 
     THETAD = v['RTOD']*THETA
     QD = v['RTOD']*Q
 
     # Bereken dinamiese druk
     QBAR = adc(VT, H, 0)
-    QS = QBAR*S 
-    SALP = math.sin(x(1))
-    CALP = math.cos(x(1))
+    QS = QBAR*v['S'] 
+    SALP = math.sin(x[1])
+    CALP = math.cos(x[1])
     STH = math.sin(THETA)
     CTH = math.cos(THETA)
-    GAM = THETA - x(1)
+    GAM = THETA - x[1]
 
     if land:
-        CL0 = 1.0
-        CD0 = 0.08
-        CM0 = -0.20
-        DCDG = 0.02
-        DCMG = -0.05
+        v['CL0'] = 1.0
+        v['CD0'] = 0.08
+        v['CM0'] = -0.20
+        v['DCDG'] = 0.02
+        v['DCMG'] = -0.05
     else:
-        CL0 = 0.20
-        CD0 = 0.016
-        CM0 = 0.05
-        DCDG = 0.0
-        DCMG = 0.0
+        v['CL0'] = 0.20
+        v['CD0'] = 0.016
+        v['CM0'] = 0.05
+        v['DCDG'] = 0.0
+        v['DCMG'] = 0.0
 
     # Stukrag (thrust)
     THR = (v['TSTAT'] + VT*v['DTDV'])*THTL
     # Hefkragkoeffisient (Lift coefficient)
-    CL = CL0 + v['CLA']*ALPHA
+    CL = v['CL0'] + v['CLA']*ALPHA
     # Heimoment (pitch moment)
-    CM = DCMG + v['CM0'] + v['CMA']*ALPHA + v['CMDE']*ELEV + CL*(XCG - 0.25)
+    CM = v['DCMG'] + v['CM0'] + v['CMA']*ALPHA + v['CMDE']*ELEV + CL*(v['XCG'] - 0.25)
     # Sleurkrag (drag polar)
-    CD = DCDG + CD0 + v['CDCLS']*CL*CL
+    CD = v['DCDG'] + v['CD0'] + v['CDCLS']*CL*CL
 
     # VT rate
     xd[0] = (THR*CALP - QS*CD)/v['AM'] - v['G']*math.sin(GAM)
@@ -94,30 +115,63 @@ def f(v, t, x, xd, land, THTL, ELEV):
     D = 0.5*v['CBAR']*(v['CMQ']*Q + v['CMADOT']*x[1])/VT
 
     # Pitch acceleration
-    xd[3] = (QS*v['CBAR']*(CM + D) + THR*v['ZE')/v['AIYY']]
+    xd[3] = (QS*v['CBAR']*(CM + D) + THR*v['ZE'])/v['AIYY']
 
     # Upward speed
     xd[4] = VT*(CALP*STH - SALP*CTH)
 
     # Horizontal speed
-    xd[5] = VT(CALP*CTH + SALP*STH)
+    xd[5] = VT*(CALP*CTH + SALP*STH)
 
     # Normal acceleration
-    AN = QS*(v['CL']*CALP + CD*SALP)/(v['G']*v['AM'])
+    AN = QS*(CL*CALP + CD*SALP)/(v['G']*v['AM'])
 
     return xd
 
 
+#%% 
+# f(x, t, v, land, THTL, ELEV)
+x = [100,0,0,0,0,0]
 
+print(f(x, 0, vliegtuig, False, 0, 0))
 
-#%% Roep funksie
+#%% Integreer funksie
 
-t = 0 # Tyd inisialiseer
-# Toestandsveranderlike model (state space model)
-x = np.array([1, 2, 3, 4, 5])
-# Afgeleide van model
-xd = np.array([1, 2, 3, 4, 5])
+x0 = [100,0,0,0,0,0]
 
-# Roep die funksie
-print(f(vliegtuig, t, x, xd), True, 0, 0)
-# %%
+t = np.linspace(0, 10, 101)
+sol = odeint(f, x0, t, args=(vliegtuig, False, 0, 0))
+
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
+
+#%%
+# Voorbeeld integrasie
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
+
+def pend(y, t, b, c):
+    theta, omega = y
+    dydt = [omega, -b*omega - c*np.sin(theta)]
+
+    return dydt
+
+b = 0.25
+c = 5.0
+
+y0 = [np.pi - 0.1, 0.0]
+
+t = np.linspace(0, 10, 101)
+sol = odeint(pend, y0, t, args=(b, c))
+
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
