2337cc966ef113943a78cf2dbd50f976c15444fd
NielAgenbag <niel.agenbag@vulpes-tech.co.za>
2025-01-05 23:19:23 +0200

    Sit Jakobiaan berekening by met voorbeeld

--- AircraftControlToolbox.py
+++ AircraftControlToolbox.py
@@ -1,8 +1,11 @@
 #%% Bevat al die algortimes benodig vir die programme in Stevens
 import matplotlib.pyplot as plt
+import math as math
+import numpy as np
+
 
 #%%
-def rk4(f, dt, xx, nx):
+def rk4(f, dt, xx, nx, u):
     # Runge Kutta 4de orde:
     # f - funksie wat integreer moet word.
     # tt - huidige tyd
@@ -11,7 +14,7 @@ def rk4(f, dt, xx, nx):
     # xd - afgeleide van xx vektor met betrekking tot tyd
     # nx - aantal veranderlikes in toestandsveranderlike vektor
     
-    xd = f(xx, 0)
+    xd = f(xx, 0, u)
 
     # Inisialiseer die vektore
     x = []
@@ -22,20 +25,20 @@ def rk4(f, dt, xx, nx):
         x.append(xx[m] + 0.5*xa[m])
 
     t = dt
-    xd = f(x, t)
+    xd = f(x, t, u)
     for m in range(0, nx):
         q = xd[m]*dt
         x[m] = xx[m] + 0.5*q
         xa[m] = xa[m] + q + q
 
-    xd = f(x, t)
+    xd = f(x, t, u)
     for m in range(0, nx):
         q = xd[m]*dt
         x[m] = xx[m] + q
         xa[m] = xa[m] + q + q        
 
     tt = dt
-    xd = f(x, tt)
+    xd = f(x, tt, u)
     for m in range(0, nx):
         xx[m] = xx[m] + (xa[m] + xd[m]*dt)/6.0
 
@@ -43,13 +46,94 @@ def rk4(f, dt, xx, nx):
 
 
 
+#%%
+# Kyk na NumerieseJakobiaan.md vir notas
+
+def numJakob(f, t, u0, x0):
+    # Funksie wat die numeriese Jakobiaan van 'n LTI (Lineer tyd onveranderlike)
+    # stelsel bereken.  Die Jakobiaan word rondom 'n gestadigde bedryfspunt
+    # bereken.    
+    # 
+    # Insette:  
+    # f         - funksie van die bewegingsvergelykings
+    # t         - tyd in [s] waar funksie evalueer word
+    # u         - beheervektor waar rondom die Jakobiaan bereken word
+    # xx        - beginwaardes waar rondom die Jakobiaan bereken word
+    
+    # 'n LTI stelsel is nie afhanklik van tyd nie, dus hou t = 0 arbriter
+    t = 0
+    
+    def njvek(f, t, u0, x0, dx, ni):
+        # Bepaal 'n toestandsveranderlike vektor van die funksie
+        # ni is die nommer van die veranderlike wat versteur word
+        # dx is die waarde waarmee die veranderlike versteur word
+
+        xd0 = f(x0, t, u0) # vektor by gestadigde toestand
+        x1 = [item for item in x0] # Inisialiseer die tweede vektor by versteurde toestand
+        
+        x1[ni] += dx
+        xd1 = f(x1, t, u0)
+
+        njvek = []
+        # Bereken die gradient vir elke veranderlike
+        for xd0el, xd1el in zip(xd0, xd1):
+            njvek.append((xd1el - xd0el)/dx)
+        
+        return njvek
+
+    def njvekkonv(f, t, u0, x0, ni):
+        # Konvergeer die Jakobiaan vektor totdat minder as 0.1% 
+        # verandering in die grootste verandering gebeur het.
+            
+        dx = 1.0
+        tolcheck = 1
+
+        while tolcheck > 0.0001:
+            njvek0 = njvek(f, t, u0, x0, dx, ni)
+            dx = dx/2
+            njvek1 = njvek(f, t, u0, x0, dx, ni)
+
+            tolerance = []
+
+            for el0, el1 in zip(njvek0, njvek1):
+                tolerance.append(abs(el1 - el0))
+
+            tolcheck = max(tolerance)
+            #print(f"{'Tolerance '}{tolcheck}{' dx '}{dx}")
+
+        njvekkonv = njvek1
+
+        return njvekkonv
+    
+    Jakobiaant = []
+    
+    for ni in range(0, len(x0)):
+        Jakobiaant.append(njvekkonv(f, t, u0, x0, ni))
+
+    # star operator will first
+    # unpack the values of 2D list
+    # and then zip function will 
+    # pack them again in opposite manner
+    Jakobiaan = list(map(list, zip(*Jakobiaant)))
+
+    return Jakobiaan
 
 
+f = pendulum
+u0 = [0]
+x0 = [0, 0]
+print('Numeriese berekening van Jakobiaan')
+print(numJakob(f, 0, u0, x0))
 
+regtewaarde = [[0, 1], [-9.81/3, 0]]
+print(f"{'Analitiese waarde van Jakobiaan '}{regtewaarde}")
+print('Hier is die eiewaardes en eievektore')
+eiewaardes, eievektore = np.linalg.eig(np.array(regtewaarde))
+print(eiewaardes)
 
 #%%
 
-def puntmassa(x, t):
+def puntmassa(x, t, u):
     # Bereken die posisie van 'n puntmassa, gegewe die beginsnelheid
     # en die versnelling.  Die versnelling in hierdie geval is 
     # swaartekrag op aarde, naamlik 9.81m/s^2
@@ -62,7 +146,7 @@ def puntmassa(x, t):
     return xd
 
 # voorbeeld van die puntmassa se toestandsveranderlike vektor:
-print(puntmassa([10, 10], 0))
+print(puntmassa([10, 10], 0, []))
 
 dt = 0.01 # sekondes
 tydvektor = [i*dt for i in range(0, 1001)]
@@ -73,7 +157,7 @@ x0 = [0, 0]
 xx =[]
 
 for tyd in tydvektor:
-    xx = rk4(puntmassa, dt, x0, len(x0))
+    xx = rk4(puntmassa, dt, x0, len(x0), [])
     afstand.append(xx[0])
     
 
@@ -87,6 +171,50 @@ plt.show()
 
 print(f"{'Antwoord moet wees '}{0.5*9.81*(0.01*1000)**2}")
 print(f"{'Antwoord moet is '}{afstand[-2]:3.8f}")
+
+
+#%%
+# Nie-lineere toestandsveranderlike stelsel wat 'n pendulum beskryf
+# Hierdie stelsel word gebruik om die numeriese Jakobiaan funksie te ontfout
+
+def pendulum(x, t, u):
+    l = 3 # [m]
+    m = 0.1 # [kg]
+
+    xd = [0, 0]
+
+    xd[0] = x[1]
+    xd[1] = -(9.81/l)*math.sin(x[0]) + (1/(l*m)*u[0]**2)
+
+    return xd
+
+
+# voorbeeld van die pendulum se toestandsveranderlike vektor:
+# Rondom die ekwilibrium punt
+print(pendulum([0, 0], 0, [0]))
+
+dt = 0.01 # sekondes
+tydvektor = [i*dt for i in range(0, 1001)]
+afstand = [0]
+
+# Beginwaardes
+x0 = [0.05, 0]
+xx =[]
+u = [0.5]
+
+for tyd in tydvektor:
+    xx = rk4(pendulum, dt, x0, len(x0), u)
+    afstand.append(xx[0]*180/3.14159)
+    
+
+plt.plot(tydvektor, afstand[0:-1], 'b', label=r'$\theta$')
+
+plt.legend(loc='best')
+plt.xlabel('t [s]')
+plt.ylabel('Hoek [grade]')
+plt.grid()
+plt.show()
+
 
 
 #%%
---
+++ NumerieseJakobiaan.md
@@ -0,0 +1,103 @@
+# Numeriese Jakobiaan berekeninge
+
+Hier is 'n goeie verduideliking van die numeriese Jakobiaan:
+
+https://aleksandarhaber.com/correct-and-clear-explanation-of-linearization-of-dynamical-systems/
+
+Hier is ook 'n voorbeeld van hoe om dit in SymPy simbolies te evalueer:
+
+https://aleksandarhaber.com/symbolic-and-automatic-linearization-of-nonlinear-systems-in-python-by-using-sympy-library/
+
+
+
+Die numeriese Jakobiaan word gebruik vir linearisasie van die bewegingsvergelykings rondom 'n bedryfspunt wat gewoonlik 'n gestadigde toestand is.  Die eiewaardes van die $A$ matriks word ook gebruik om die natuurlike frekwensie en dempingsverhoudings van die stelsel rondom die bedryfspunt te bereken.
+
+
+
+Die Jakobiaan van 'n toestandsveranderlike vektor word gedefinieer as:
+
+
+$$
+A = \frac{\partial \mathbf{f}}{\partial \mathbf{x}} = 
+\begin{bmatrix}
+\frac{\partial f_1}{\partial x_1} & \frac{\partial f_1}{\partial x_2} & \ldots & \frac{\partial f_1}{\partial x_n} \\
+\frac{\partial f_2}{\partial x_1} & \frac{\partial f_2}{\partial x_2} & \ldots & \frac{\partial f_2}{\partial x_n} \\
+\vdots & \vdots &  & \vdots \\
+\frac{\partial f_n}{\partial x_1} & \frac{\partial f_n}{\partial x_2} & \ldots & \frac{\partial f_n}{\partial x_n}
+\end{bmatrix}
+,
+B = \frac{\partial \mathbf{f}}{\partial \mathbf{u}} = 
+\begin{bmatrix}
+\frac{\partial f_1}{\partial u_1} & \frac{\partial f_1}{\partial u_2} & \ldots & \frac{\partial f_1}{\partial u_m} \\
+\frac{\partial f_2}{\partial u_1} & \frac{\partial f_2}{\partial u_2} & \ldots & \frac{\partial f_2}{\partial u_m} \\
+\vdots & \vdots &  & \vdots \\
+\frac{\partial f_n}{\partial u_1} & \frac{\partial f_n}{\partial u_2} & \ldots & \frac{\partial f_n}{\partial u_m}
+\end{bmatrix}
+$$
+
+
+Skryf 'n python funksie sodat die volgende vektor vir elke veranderlike bereken word tot 'n toleransie van 0.001.
+
+
+$$
+\frac{\partial \mathbf{f}}{\partial \mathbf{x_n}} = 
+\begin{bmatrix}
+\frac{\partial f_1}{\partial x_n}  \\
+\frac{\partial f_2}{\partial x_n}  \\
+\vdots \\
+\frac{\partial f_n}{\partial x_n}  
+\end{bmatrix}
+$$
+
+
+Gebruik die pendulum toestandsveranderlike model om die Jakobiaan funksie te ontfout/toets.  Die pendulum se bewegingsvergelykings is as volg:
+
+
+$$
+\begin{bmatrix}
+\dot{x_1}  \\
+\dot{x_2} 
+\end{bmatrix}
+=
+\begin{bmatrix}
+f_1  \\
+f_2 
+\end{bmatrix}
+=
+\begin{bmatrix}
+x_2  \\
+-\frac{g}{l} \sin(x_1) + \frac{1}{ml} u^2
+\end{bmatrix}
+$$
+waar $l$ die lengte van die pendulum is en $m$ die massa is.
+
+waar $\dot{x_1} = \dot{\theta}$ en $\dot{x_2} = \ddot{\theta}$ en $\theta$ is die hoek van die pendulum tou met betrekking tot die vertikale as.
+
+Die simboliese evaluering van die Jakobiaan van hierdie stelsel is as volg:
+$$
+\frac{\partial \mathbf{f}}{\partial \mathbf{x}} = 
+\begin{bmatrix}
+\frac{\partial f_1}{\partial x_1} & \frac{\partial f_1}{\partial x_2} \\
+\frac{\partial f_2}{\partial x_1} & \frac{\partial f_2}{\partial x_2} \\
+\end{bmatrix} = 
+\begin{bmatrix}
+0 & 1 \\
+-\frac{g}{l} \cos(x_1) & 0 \\
+\end{bmatrix}
+$$
+Vir die ekwilibrium punt van $\begin{bmatrix} x_1 \\ x_2\end{bmatrix} = \begin{bmatrix} 0 \\ 0\end{bmatrix}$ , of waar die pedulum stil vertikaal hang, is die waarde van die Jakobiaan:
+$$
+\frac{\partial \mathbf{f}}{\partial \mathbf{x}} = 
+\begin{bmatrix}
+0 & 1 \\
+-\frac{g}{l} \cos(x_1) & 0 \\
+\end{bmatrix} = 
+\begin{bmatrix}
+0 & 1 \\
+-\frac{9.81 m/s^2}{3m} \cos(0^\circ) & 0 \\
+\end{bmatrix} =
+\begin{bmatrix}
+0 & 1 \\
+-3.27 & 0 \\
+\end{bmatrix}
+$$
